7433e8bfcbe586bb8caca81137bf9f091a4fe1a9
MomDra <yem9087@koreatech.ac.kr>
2021-05-16 21:46:51 +0900

    Modify memberinfo

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>MomDraStudio</title>
+    <link rel="stylesheet" type="text/css" href="./css/common.css">
+    <link rel="stylesheet" type="text/css" href="./css/main.css">
+</head>
+<body>
+    <header>
+    	<?php include "header.php";?>
+    </header>
+	<section>
+	    <?php include "main.php";?>
+	</section> 
+	<footer>
+    	<?php include "footer.php";?>
+    </footer>
+</body>
+</html>
